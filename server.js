693e0ce6fe06a3255cfa95c538d6a65a622d685f
C12 <mcpartlandpatrick@gmail.com>
2023-05-31 15:00:02 -0400

    naming changes, updated scripts in index/notes.html

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('.db/db.json', (req, res) => res.json(notes));
+// app.get('/api/notes', (req, res) => res.json(notes));
+
+app.get('/notes', (req, res)=> {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
