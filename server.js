13f5639701daad5866e2583d936f861e70471c2e
C12 <mcpartlandpatrick@gmail.com>
2023-05-31 14:23:34 -0400

    rearranged folders, set server to root of file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db.json');
+const notes = require('./db/db.json');
 const PORT = 3001;
 
 const app = express();
@@ -8,10 +8,10 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('./db.json', (req, res) => res.json(notes));
+app.get('.db/db.json', (req, res) => res.json(notes));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
